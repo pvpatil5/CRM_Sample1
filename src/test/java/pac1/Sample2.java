d5865245fba00149573978d0c8c9fcd8f57bd5a5
pvpatil5 <pvpatil123@yahoo.com>
2020-08-13 15:09:30 +0530

    CRM First Commit

---
+++ src/test/java/pac1/Sample2.java
@@ -0,0 +1,18 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class Sample2 {
+	@Test
+	public  void add()
+	{
+
+		int a = 5;
+		int b = 10;
+
+		System.out.println("Ans="+ (a+b));
+
+
+	}
+
+}
